0682384a2a569d7918f7cbcd0d94e56b1d5a57a8
hop- <hovhannespalyan@gmail.com>
2022-08-21 16:21:25 +0400

    Add debug channel and support of logging modes

--- init.go
+++ init.go
@@ -8,20 +8,41 @@ import (
 
 var (
 	file       *os.File
+	debugLog   *log.Logger
 	infoLog    *log.Logger
 	warningLog *log.Logger
 	errorLog   *log.Logger
 )
 
+type LogMode = int
+
+const (
+	ErrorMode LogMode = iota + 1
+	WarningMode
+	InfoMode
+	DebugMode
+)
+
 // Init initiate output streams
-func Init() {
-	infoLog = log.New(os.Stdout, "Info: ", log.Ldate|log.Ltime)
-	warningLog = log.New(os.Stderr, "Warning: ", log.Ldate|log.Ltime)
-	errorLog = log.New(os.Stderr, "Error: ", log.Ldate|log.Ltime)
+func Init(mode string) {
+	switch getMode(mode) {
+	case DebugMode:
+		debugLog = log.New(os.Stdout, "Debug: ", log.Ldate|log.Ltime)
+		fallthrough
+	case InfoMode:
+		infoLog = log.New(os.Stdout, "Info: ", log.Ldate|log.Ltime)
+		fallthrough
+	case WarningMode:
+		warningLog = log.New(os.Stderr, "Warning: ", log.Ldate|log.Ltime)
+		fallthrough
+	default:
+		errorLog = log.New(os.Stderr, "Error: ", log.Ldate|log.Ltime)
+	}
+
 }
 
 // InitFile initiate output streams and setup log file
-func InitFile(fileName string) {
+func InitFile(fileName string, mode string) {
 	var err error
 
 	file, err = os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
@@ -31,7 +52,31 @@ func InitFile(fileName string) {
 
 	stdOutWriter := io.MultiWriter(file, os.Stdout)
 	stdErrWriter := io.MultiWriter(file, os.Stderr)
-	infoLog = log.New(stdOutWriter, "Info: ", log.Ldate|log.Ltime)
-	warningLog = log.New(stdErrWriter, "Warning: ", log.Ldate|log.Ltime)
-	errorLog = log.New(stdErrWriter, "Error: ", log.Ldate|log.Ltime)
+
+	switch getMode(mode) {
+	case DebugMode:
+		debugLog = log.New(stdOutWriter, "Debug: ", log.Ldate|log.Ltime)
+		fallthrough
+	case InfoMode:
+		infoLog = log.New(stdOutWriter, "Info: ", log.Ldate|log.Ltime)
+		fallthrough
+	case WarningMode:
+		warningLog = log.New(stdErrWriter, "Warning: ", log.Ldate|log.Ltime)
+		fallthrough
+	default:
+		errorLog = log.New(stdErrWriter, "Error: ", log.Ldate|log.Ltime)
+	}
+}
+
+func getMode(mode string) LogMode {
+	switch mode {
+	case "WARNING":
+		return WarningMode
+	case "ERROR":
+		return ErrorMode
+	case "DEBUG":
+		return DebugMode
+	default:
+		return WarningMode
+	}
 }
--- log.go
+++ log.go
@@ -4,49 +4,83 @@ import (
 	"os"
 )
 
+// Debug log debug line
+func Debug(v ...interface{}) {
+	if debugLog == nil {
+		return
+	}
+	debugLog.Println(v...)
+}
+
 // Info log info line
 func Info(v ...interface{}) {
+	if infoLog == nil {
+		return
+	}
 	infoLog.Println(v...)
 }
 
 // Warning log warning line
 func Warning(v ...interface{}) {
+	if warningLog == nil {
+		return
+	}
 	warningLog.Println(v...)
 }
 
 // Error log error line
 func Error(v ...interface{}) {
+	if errorLog == nil {
+		return
+	}
 	errorLog.Println(v...)
-}
-
-// Infof log info line with format
-func Infof(format string, v ...interface{}) {
-	infoLog.Printf(format, v...)
-}
-
-// Warningf log warning line with format
-func Warningf(format string, v ...interface{}) {
-	warningLog.Printf(format, v...)
-}
-
-// Errorf log error line with format
-func Errorf(format string, v ...interface{}) {
-	errorLog.Printf(format, v...)
 }
 
 // Fatal log fatal error and terminate
 func Fatal(v ...interface{}) {
 	Error(v...)
 	Error("Fatal error")
-    Close()
+	Close()
 	os.Exit(1)
+}
+
+// Debugf log debug line with format
+func Debugf(format string, v ...interface{}) {
+	if debugLog == nil {
+		return
+	}
+	debugLog.Printf(format, v...)
+}
+
+// Infof log info line with format
+func Infof(format string, v ...interface{}) {
+	if infoLog == nil {
+		return
+	}
+	infoLog.Printf(format, v...)
+}
+
+// Warningf log warning line with format
+func Warningf(format string, v ...interface{}) {
+	if warningLog == nil {
+		return
+	}
+	warningLog.Printf(format, v...)
+}
+
+// Errorf log error line with format
+func Errorf(format string, v ...interface{}) {
+	if errorLog == nil {
+		return
+	}
+	errorLog.Printf(format, v...)
 }
 
 // Fatalf log fatal error with format
 func Fatalf(format string, v ...interface{}) {
-    Errorf(format, v...)
+	Errorf(format, v...)
 	Error("Fatal error")
-    Close()
+	Close()
 	os.Exit(1)
 }
 
